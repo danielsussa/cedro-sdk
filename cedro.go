533e2f22c988e54804c7fc608c2c7f74090fa6cc
Daniel Kanczuk <danielsussa@gmail.com>
2025-07-23 14:28:35 -0300

    first commit

---
+++ cedro.go
@@ -0,0 +1,305 @@
+package cedro
+
+import (
+	"slices"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type (
+	Symbols struct {
+		Symbols []*Symbol
+	}
+
+	Symbol struct {
+		Symbol          string
+		Bid             float64
+		Ask             float64
+		Last            float64
+		TheoreticalRate float64
+		Participation   float64
+
+		BookLineAsk []*BookLine
+		BookLineBid []*BookLine
+
+		AggregatedBookAsk []*AggregatedBook
+		AggregatedBookBid []*AggregatedBook
+	}
+
+	AggregatedBook struct {
+		Price       float64
+		Volume      int
+		TotalOrders int
+		DT          time.Time
+	}
+
+	BookLine struct {
+		Price  float64
+		Volume int
+		Broker int
+		DT     time.Time
+	}
+)
+
+func Process(msg string, symbols []*Symbol) []*Symbol {
+	msgSpl := strings.Split(msg, ":")
+	switch msgSpl[0] {
+	case "T":
+		var symbol *Symbol
+		symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+		for i := 3; i < len(msgSpl); i += 2 {
+			switch msgSpl[i] {
+			case "2": // Preço do último negócio
+				price := stringToFloat(msgSpl[4])
+				symbol.Last = price
+			case "3": // Melhor oferta de compra
+				price := stringToFloat(msgSpl[4])
+				symbol.Bid = price
+			case "4": // Melhor oferta de venda
+				price := stringToFloat(msgSpl[4])
+				symbol.Ask = price
+			}
+		}
+	case "Z":
+		switch msgSpl[2] {
+		case "U":
+			var symbol *Symbol
+			symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+			position := stringToInt(msgSpl[3])
+			direction := msgSpl[4]
+			price := stringToFloat(msgSpl[5])
+			volume := stringToInt(msgSpl[6])
+			totalOrders := stringToInt(msgSpl[7])
+
+			bl := &AggregatedBook{
+				Price:       price,
+				Volume:      volume,
+				TotalOrders: totalOrders,
+			}
+			if direction == "V" {
+				symbol.AggregatedBookAsk = addOnPositionAgr(symbol.AggregatedBookAsk, bl, position)
+			} else {
+				symbol.AggregatedBookBid = addOnPositionAgr(symbol.AggregatedBookBid, bl, position)
+			}
+		case "A":
+			var symbol *Symbol
+			symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+			position := stringToInt(msgSpl[3])
+			direction := msgSpl[4]
+			price := stringToFloat(msgSpl[5])
+			volume := stringToInt(msgSpl[6])
+			totalOrders := stringToInt(msgSpl[7])
+
+			bl := &AggregatedBook{
+				Price:       price,
+				Volume:      volume,
+				TotalOrders: totalOrders,
+			}
+			if direction == "V" {
+				symbol.AggregatedBookAsk = addOnPositionAgr(symbol.AggregatedBookAsk, bl, position)
+			} else {
+				symbol.AggregatedBookBid = addOnPositionAgr(symbol.AggregatedBookBid, bl, position)
+			}
+		case "D":
+			switch msgSpl[3] {
+			case "1": // O tipo 1 indica que somente a oferta da posição indicada deve ser cancelada.
+				var symbol *Symbol
+				symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+				position := stringToInt(msgSpl[3])
+				direction := msgSpl[4]
+				if direction == "A" {
+					symbol.BookLineBid = removeAt(symbol.BookLineBid, position)
+				} else {
+					symbol.BookLineAsk = removeAt(symbol.BookLineAsk, position)
+				}
+
+			case "2": // O tipo 2 indica que todas as ofertas melhores do que a oferta indicada pela posição, inclusive ela, devem ser canceladas.
+				var symbol *Symbol
+				symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+				position := stringToInt(msgSpl[3])
+				direction := msgSpl[4]
+				if direction == "A" {
+					symbol.BookLineBid = removeFirstN(symbol.BookLineBid, position)
+				} else {
+					symbol.BookLineAsk = removeFirstN(symbol.BookLineAsk, position)
+				}
+			case "3": // O tipo 3 indica que todas as ofertas, tanto de compra quanto de venda, devem ser canceladas. Neste tipo a mensagem não vem acompanhada de direção e posição.
+				var symbol *Symbol
+				symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+				symbol.BookLineBid = make([]*BookLine, 0)
+				symbol.BookLineAsk = make([]*BookLine, 0)
+			}
+		}
+	case "B":
+		switch msgSpl[2] {
+		case "A": // process BQT ADD
+			var symbol *Symbol
+			symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+			position := stringToInt(msgSpl[3])
+			direction := msgSpl[4]
+			price := stringToFloat(msgSpl[5])
+			volume := stringToInt(msgSpl[6])
+			broker := stringToInt(msgSpl[7])
+			dt := stringToTime(msgSpl[8])
+
+			bl := &BookLine{
+				Price:  price,
+				Volume: volume,
+				Broker: broker,
+				DT:     dt,
+			}
+			if direction == "A" {
+				symbol.BookLineBid = addOnPosition(symbol.BookLineBid, bl, position)
+			} else {
+				symbol.BookLineAsk = addOnPosition(symbol.BookLineAsk, bl, position)
+			}
+		case "D": // process BQT DELETE
+			switch msgSpl[3] {
+			case "1": // O tipo 1 indica que somente a oferta da posição indicada deve ser cancelada.
+				var symbol *Symbol
+				symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+				position := stringToInt(msgSpl[3])
+				direction := msgSpl[4]
+				if direction == "A" {
+					symbol.BookLineBid = removeAt(symbol.BookLineBid, position)
+				} else {
+					symbol.BookLineAsk = removeAt(symbol.BookLineAsk, position)
+				}
+
+			case "2": // O tipo 2 indica que todas as ofertas melhores do que a oferta indicada pela posição, inclusive ela, devem ser canceladas.
+				var symbol *Symbol
+				symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+				position := stringToInt(msgSpl[3])
+				direction := msgSpl[4]
+				if direction == "A" {
+					symbol.BookLineBid = removeFirstN(symbol.BookLineBid, position)
+				} else {
+					symbol.BookLineAsk = removeFirstN(symbol.BookLineAsk, position)
+				}
+			case "3": // O tipo 3 indica que todas as ofertas, tanto de compra quanto de venda, devem ser canceladas. Neste tipo a mensagem não vem acompanhada de direção e posição.
+				var symbol *Symbol
+				symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+				symbol.BookLineBid = make([]*BookLine, 0)
+				symbol.BookLineAsk = make([]*BookLine, 0)
+			}
+		case "U": // process BQT UPDATE
+			var symbol *Symbol
+			symbols, symbol = getBySymbol(msgSpl[1], symbols)
+
+			newPosition := stringToInt(msgSpl[3])
+			oldPosition := stringToInt(msgSpl[4])
+			direction := msgSpl[5]
+			price := stringToFloat(msgSpl[6])
+			volume := stringToInt(msgSpl[7])
+			broker := stringToInt(msgSpl[8])
+			dt := stringToTime(msgSpl[9])
+
+			bl := &BookLine{
+				Price:  price,
+				Volume: volume,
+				Broker: broker,
+				DT:     dt,
+			}
+			if direction == "V" {
+				symbol.BookLineAsk = removeAt(symbol.BookLineAsk, oldPosition)
+				symbol.BookLineAsk = addOnPosition(symbol.BookLineAsk, bl, newPosition)
+			} else {
+				symbol.BookLineBid = removeAt(symbol.BookLineBid, oldPosition)
+				symbol.BookLineBid = addOnPosition(symbol.BookLineBid, bl, newPosition)
+			}
+
+		}
+	}
+
+	return symbols
+}
+
+func getBySymbol(symbol string, list []*Symbol) ([]*Symbol, *Symbol) {
+	idx := slices.IndexFunc(list, func(i *Symbol) bool {
+		return i.Symbol == symbol
+	})
+	if idx == -1 {
+		s := &Symbol{Symbol: symbol}
+		list = append(list, s)
+		return list, s
+	}
+
+	return list, list[idx]
+}
+
+func stringToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return i
+}
+
+func stringToFloat(s string) float64 {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0
+	}
+	return f
+}
+
+func stringToTime(s string) time.Time {
+	// 22071052
+	s = "2025" + s
+	dt, _ := time.Parse("200602011504", s)
+	return dt
+}
+
+func addOnPosition(list []*BookLine, bl *BookLine, pos int) []*BookLine {
+	if list == nil {
+		list = make([]*BookLine, 0)
+	}
+	for {
+		if len(list) > pos {
+			break
+		}
+		list = append(list, nil)
+	}
+	list[pos] = bl
+	return list
+}
+
+func addOnPositionAgr(list []*AggregatedBook, bl *AggregatedBook, pos int) []*AggregatedBook {
+	if list == nil {
+		list = make([]*AggregatedBook, 0)
+	}
+	for {
+		if len(list) > pos {
+			break
+		}
+		list = append(list, nil)
+	}
+	list[pos] = bl
+	return list
+}
+
+func removeFirstN(list []*BookLine, pos int) []*BookLine {
+	for i := pos - 1; i >= 0; i-- {
+		list[i] = nil
+	}
+
+	return list
+}
+func removeAt(list []*BookLine, pos int) []*BookLine {
+	if len(list) <= pos {
+		return list
+	}
+	list[pos] = nil
+	return list
+}
---
+++ cedro_test.go
@@ -0,0 +1,24 @@
+package cedro
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_multiApi(t *testing.T) {
+	t.Run("basic book operations", func(t *testing.T) {
+		msgs := []string{
+			"Z:WINQ25:U:9:V:135420:598:224:22071453",
+			"Z:WINQ25:A:0:A:135385:4:1:22071455",
+			"Z:WINQ25:U:9:V:135420:595:224:22071453",
+			"Z:WINQ25:A:5:A:135395:40:1:22071455",
+		}
+
+		symbols := make([]*Symbol, 0)
+		for _, msg := range msgs {
+			symbols = Process(msg, symbols)
+		}
+		assert.Equal(t, 595, symbols[0].AggregatedBookAsk[9].Volume)
+		assert.Equal(t, 4, symbols[0].AggregatedBookBid[0].Volume)
+	})
+}
